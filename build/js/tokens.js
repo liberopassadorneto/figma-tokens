f68b67baf568887ffa9a712219040145a570814c
libero <liberopneto@gmail.com>
2022-11-29 19:05:40 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 29 Nov 2022 22:02:24 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#6b7280",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple600": "#9333ea"
+};
